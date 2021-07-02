88546c01fc7a1f1f3364f93c7a314528d503bab8
Subho <subhadipghosh215@gmail.com>
2021-07-03 04:48:51 +0530

    commit

--- pages/indexparts/Role.js
+++ pages/indexparts/Role.js
@@ -90,7 +90,7 @@ const Role = () => {
                 <div className="row">
 
                     <div className="col-12 text-center">
-                        <p className="py-3 m-0">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                        <p className="py-3 m-0">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                         <div className="main-blue-button-hover pt-4"><Link href="#"><a>Search</a></Link></div>
                     </div>
 
